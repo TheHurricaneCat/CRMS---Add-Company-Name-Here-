3fdef177089b5ac96866b50b6f2e7409597b44a0
gabrieljamesbenedict <benok.loslos@gmail.com>
2024-07-24 06:21:02 +0800

    Removed modelList and addModeltoList

--- src/com/group5/Car/Make.java
+++ src/com/group5/Car/Make.java
@@ -6,17 +6,11 @@ public class Make {
     
     private String name;
     private String country;
-    private ArrayList<Model> modelList;
 
 
     public Make (String name, String country) {
         this.name = name;
         this.country = country;
-    }
-
-
-    public void addModeltoList (Model model) {
-        modelList.add(model);
     }
 
     public String getName() {
