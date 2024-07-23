af4235c7e2236232843f2914f77b7936ad319dfb
gabrieljamesbenedict <benok.loslos@gmail.com>
2024-07-24 02:08:02 +0800

    Added Builder behavior to Engine class

--- src/com/group5/Car/Parts/Engine.java
+++ src/com/group5/Car/Parts/Engine.java
@@ -10,4 +10,40 @@ public class Engine {
     private double highwayFuelEconomy;
     private String pistonConfiguration;
 
+
+    public Engine setEngineDisplacement(double engineDisplacement) {
+        this.engineDisplacement = engineDisplacement;
+        return this;
+    }
+
+    public Engine setHorsepower(int horsepower) {
+        this.horsepower = horsepower;
+        return this;
+    }
+
+    public Engine setTorque(int torque) {
+        this.torque = torque;
+        return this;
+    }
+
+    public Engine setFuelType(String fuelType) {
+        this.fuelType = fuelType;
+        return this;
+    }
+
+    public Engine setCityFuelEconomy(double cityFuelEconomy) {
+        this.cityFuelEconomy = cityFuelEconomy;
+        return this;
+    }
+
+    public Engine setHighwayFuelEconomy(double highwayFuelEconomy) {
+        this.highwayFuelEconomy = highwayFuelEconomy;
+        return this;
+    }
+
+    public Engine setPistonConfiguration(String pistonConfiguration) {
+        this.pistonConfiguration = pistonConfiguration;
+        return this;
+    }
+
 }
