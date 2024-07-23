4bb32970420b6ff3a5a786d0406bf65ca9cd06e4
gabrieljamesbenedict <benok.loslos@gmail.com>
2024-07-24 00:29:01 +0800

    Added Part Subclass 'Body'

---
+++ src/com/group5/Car/Parts/Body.java
@@ -0,0 +1,12 @@
+package com.group5.Car.Parts;
+
+public class Body {
+    
+    private double grossWeight;
+    private double curbWeight;
+    private double cargoCapacity;
+    private double towingCapacity;
+    private double seatingCapacity;
+    private String color;
+    
+}
