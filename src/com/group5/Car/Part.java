a396a1d6aa6b9615468a2f3953c143f5e8d1d0b5
gabrieljamesbenedict <benok.loslos@gmail.com>
2024-07-24 02:39:48 +0800

    Added setters for Part class

--- src/com/group5/Car/Part.java
+++ src/com/group5/Car/Part.java
@@ -4,5 +4,13 @@ public class Part {
     
     private String name;
     private String material;
+
+    void setName (String name) {
+        this.name = name;
+    }
+
+    void setMaterial (String material) {
+        this.material = material;
+    }
     
 }
