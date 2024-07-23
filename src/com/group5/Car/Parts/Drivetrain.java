ee7d2e8f366248bf9fa089ffcb5120dd5ed2e335
gabrieljamesbenedict <benok.loslos@gmail.com>
2024-07-24 02:24:44 +0800

    Added Builer bahviour to Drivetrain class

--- src/com/group5/Car/Parts/Drivetrain.java
+++ src/com/group5/Car/Parts/Drivetrain.java
@@ -5,5 +5,18 @@ public class Drivetrain {
     private String wheelDriveType;
     private double finalDriveRatio;
     private double differentialRatio;
+
+    public Drivetrain setWheelDriveType(String wheelDriveType) {
+        this.wheelDriveType = wheelDriveType;
+        return this;
+    }
+    public Drivetrain setFinalWheelDrive(double finalDriveRatio) {
+        this.finalDriveRatio = finalDriveRatio;
+        return this;
+    }
+    public Drivetrain setDifferentialRatio(double differentialRatio) {
+        this.differentialRatio = differentialRatio;
+        return this;
+    }
     
 }
