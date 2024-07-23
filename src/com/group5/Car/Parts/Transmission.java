639e4c59c9f5391427b50057a04f2444eaa7f9fc
gabrieljamesbenedict <benok.loslos@gmail.com>
2024-07-24 02:11:57 +0800

    Added Builder behavior to Transmission class

--- src/com/group5/Car/Parts/Transmission.java
+++ src/com/group5/Car/Parts/Transmission.java
@@ -5,5 +5,21 @@ public class Transmission {
     private String transmissionType;
     private int gearCount;
     private Boolean torqueConverter;
+
+
+    public Transmission setTransmissionType(String transmissionType) {
+        this.transmissionType = transmissionType;
+        return this;
+    }
+
+    public Transmission setGearCount(int gearCount) {
+        this.gearCount = gearCount;
+        return this;
+    }
+
+    public Transmission setTorqueConverter(Boolean torqueConverter) {
+        this.torqueConverter = torqueConverter;
+        return this;
+    }
     
 }
