4e1b9c8af990cf1382005b9388345a94288ad570
gabrieljamesbenedict <benok.loslos@gmail.com>
2024-07-24 00:55:52 +0800

    Added CarBuilder class

---
+++ src/com/group5/Car/CarBuilder.java
@@ -0,0 +1,42 @@
+package com.group5.Car;
+
+import java.util.ArrayList;
+
+class CarBuilder {
+
+    private int carID;
+    private Make make;
+    private Model model;
+    private Specifications specs;
+    private ArrayList<Part> part = new ArrayList<>();
+
+    public CarBuilder setCarID (int carID) {
+        this.carID = carID;
+        return this;
+    }
+
+    public CarBuilder setMake (Make make) {
+        this.make = make;
+        return this;
+    }
+
+    public CarBuilder setModel (Model model) {
+        this.model = model;
+        return this;
+    }
+
+    public CarBuilder setSpecs (Specifications specs) {
+        this.specs = specs;
+        return this;
+    }
+
+    public CarBuilder addPart (Part part) {
+        this.part.add(part);
+        return this;
+    }
+
+    public Car build () {
+        return new Car(carID, make, model, specs, part);
+    }
+
+}
