33394baac165ff5fb5f1baec52485a5ba49b114b
gabrieljamesbenedict <benok.loslos@gmail.com>
2024-07-24 00:47:10 +0800

    Removed 'name' variable from Car

--- src/com/group5/Car/Car.java
+++ src/com/group5/Car/Car.java
@@ -5,10 +5,13 @@ import java.util.ArrayList;
 public class Car {
     
     private int carID;
-    private String name;
     private Make make;
     private Model model;
     private Specifications specs;
     private ArrayList<Part> part = new ArrayList<>();
+
+    public String getName() {
+        return make.getName() + " " + model.getName();
+    }
     
 }
