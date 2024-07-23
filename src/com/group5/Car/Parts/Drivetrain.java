a1b4c91d09a45dd0d66cd4ca4c8018fc0736cb9c
gabrieljamesbenedict <benok.loslos@gmail.com>
2024-07-24 00:29:22 +0800

    Added Part Subclass 'Drivetrain'

---
+++ src/com/group5/Car/Parts/Drivetrain.java
@@ -0,0 +1,9 @@
+package com.group5.Car.Parts;
+
+public class Drivetrain {
+    
+    private String wheelDriveType;
+    private double finalDriveRatio;
+    private double differentialRatio;
+    
+}
