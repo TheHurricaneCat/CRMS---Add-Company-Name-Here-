aa42b2af626f24e20ebf2893d51066f8974cea7e
gabrieljamesbenedict <benok.loslos@gmail.com>
2024-07-24 00:29:39 +0800

    Added Part Subclass 'Engine'

---
+++ src/com/group5/Car/Parts/Engine.java
@@ -0,0 +1,13 @@
+package com.group5.Car.Parts;
+
+public class Engine {
+    
+    private double engineDisplacement;
+    private int hoursepower;
+    private int torque;
+    private String fuelType;
+    private double cityFuelEconomy;
+    private double highwayFuelEconomy;
+    private String pistonConfiguration;
+    
+}
