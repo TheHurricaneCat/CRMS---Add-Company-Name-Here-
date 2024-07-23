690d11aae2b798e9f1e5b6e3b2665d20bfa22abe
gabrieljamesbenedict <benok.loslos@gmail.com>
2024-07-24 02:36:55 +0800

    Added Builder behaviour to Body class

--- src/com/group5/Car/Parts/Body.java
+++ src/com/group5/Car/Parts/Body.java
@@ -9,5 +9,41 @@ public class Body {
     private double seatingCapacity;
     private String color;
     private double fuelCapacity;
+
+
+    public Body setGrossWeight(double grossWeight) {
+        this.grossWeight = grossWeight;
+        return this;
+    }
+
+    public Body setCurbWeight(double curbWeight) {
+        this.curbWeight = curbWeight;
+        return this;
+    }
+
+    public Body setCargoCapacity(double cargoCapacity) {
+        this.cargoCapacity = cargoCapacity;
+        return this;
+    }
+
+    public Body setTowingCapacity(double towingCapacity) {
+        this.towingCapacity = towingCapacity;
+        return this;
+    }
+
+    public Body setSeatingCapacity(double seatingCapacity) {
+        this.seatingCapacity = seatingCapacity;
+        return this;
+    }
+
+    public Body setColor(String color) {
+        this.color = color;
+        return this;
+    }
+
+    public Body setFuelCapacity(double fuelCapacity) {
+        this.fuelCapacity = fuelCapacity;
+        return this;
+    }
     
 }
