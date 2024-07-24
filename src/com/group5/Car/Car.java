3076eb7cda11f751151df764d0ee1c9fb5ae951f
Villy Joel Almandres <Villy Joel Almandres@MSI>
2024-07-24 18:45:53 +0800

    Removed specifications class

--- src/com/group5/Car/Car.java
+++ src/com/group5/Car/Car.java
@@ -7,15 +7,13 @@ public class Car {
     private int carID;
     private Make make;
     private Model model;
-    private Specifications specs;
     private ArrayList<Part> parts = new ArrayList<>();
 
 
-    public Car (int carID, Make make, Model model, Specifications specs, ArrayList<Part> parts) {
+    public Car (int carID, Make make, Model model, ArrayList<Part> parts) {
         this.carID = carID;
         this.make = make;
         this.model = model;
-        this.specs = specs;
         this.parts = parts;
     }
 
