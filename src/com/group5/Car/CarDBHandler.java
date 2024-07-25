45f328980370c283edd2e7e5947f6493f46f610b
gabrieljamesbenedict <benok.loslos@gmail.com>
2024-07-25 18:01:24 +0800

    Added CarDBHandler class

---
+++ src/com/group5/Car/CarDBHandler.java
@@ -0,0 +1,26 @@
+package com.group5.Car;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+
+public class CarDBHandler {
+    
+    FileWriter file;
+    BufferedWriter carDBWriter;
+    BufferedReader carDBReader;
+    
+    public void insertCar (Car car) {
+
+        
+
+    }
+
+    public Car getCar (String filepath) {
+
+
+        
+        return null;
+    }
+
+}
