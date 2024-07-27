65a554f9099dd5e3222306f91582454ec00b6696
gabrieljamesbenedict <benok.loslos@gmail.com>
2024-07-28 00:50:24 +0800

    Added Price class (again)

--- src/com/group5/Car/Price.java
+++ src/com/group5/Car/Price.java
@@ -2,4 +2,23 @@ package com.group5.Car;
 
 public class Price {
     
+    double totalPrice;
+    double partialPrice;
+    double rentPricePerDay;
+    int rentDuration;
+
+    public Price (double rentPricePerDay, int rentDuration) {
+        this.rentPricePerDay = rentPricePerDay;
+        this.rentDuration = rentDuration;
+        calculateTotalPrice();
+    }
+
+    public void calculateTotalPrice () {
+        totalPrice = rentPricePerDay * rentDuration;
+    }
+
+    public void calculatePartialPrice (int days) {
+        partialPrice = rentPricePerDay * days;
+    }
+
 }
