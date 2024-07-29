b2ff016760082195f4eff24abebfbaee9c8eea60
TheHurricaneCat <thc12895nth@gmail.com>
2024-07-29 10:41:59 +0800

    Added separator guard for registering

--- src/com/group5/Login/RegisterPanel.java
+++ src/com/group5/Login/RegisterPanel.java
@@ -215,33 +215,38 @@ public class RegisterPanel extends javax.swing.JFrame {
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
         String firstPass = new String(jPasswordField1.getPassword());
         String confirmPass = new String(jPasswordField2.getPassword());
-        if (firstPass.equals(confirmPass)) {
-            UserDBHandler handler = UserDBHandler.getInstance();
-            if (handler.userExists(jTextField1.getText())) {
-                JOptionPane.showMessageDialog(null, "User already exists... Try a different name.", "CRMS Registration", JOptionPane.WARNING_MESSAGE);
-            } else {
-                // todo: create a button or form for employees only?
-                String[] detectAdmin = jTextField1.getText().split("-");
-                int authority;
-                if (detectAdmin[1].toLowerCase().equals("admin")) {
-                    authority = 0;
+        if ((!confirmPass.contains(":") && !jTextField1.getText().contains(":"))) {
+            if (firstPass.equals(confirmPass)) {
+                UserDBHandler handler = UserDBHandler.getInstance();
+                if (handler.userExists(jTextField1.getText())) {
+                    JOptionPane.showMessageDialog(null, "User already exists... Try a different name.", "CRMS Registration", JOptionPane.WARNING_MESSAGE);
                 } else {
-                    authority = 1;
+                    // todo: create a button or form for employees only?
+                    String[] detectAdmin = jTextField1.getText().split("-");
+                    int authority;
+                    if (detectAdmin[1].toLowerCase().equals("admin")) {
+                        authority = 0;
+                    } else {
+                        authority = 1;
+                    }
+                    User user = UserFactory.createUser("1", jTextField1.getText(), new String(jPasswordField1.getPassword()));
+                    try {
+                        handler.recordUser(user);
+                    } catch (IOException ex) {
+                        Logger.getLogger(RegisterPanel.class.getName()).log(Level.SEVERE, null, ex);
+                    }
+                    JOptionPane.showMessageDialog(null, "User [" + jTextField1.getText() + "] registered successfully!", "CRMS Registration", JOptionPane.INFORMATION_MESSAGE);
+                    LoginPanel lPanel = new LoginPanel();
+                    lPanel.setVisible(true);
+                    dispose();
                 }
-                User user = UserFactory.createUser("1", jTextField1.getText(), new String(jPasswordField1.getPassword()));
-                try {
-                    handler.recordUser(user);
-                } catch (IOException ex) {
-                    Logger.getLogger(RegisterPanel.class.getName()).log(Level.SEVERE, null, ex);
-                }
-                JOptionPane.showMessageDialog(null, "User [" + jTextField1.getText() + "] registered successfully!", "CRMS Registration", JOptionPane.INFORMATION_MESSAGE);
-                LoginPanel lPanel = new LoginPanel();
-                lPanel.setVisible(true);
-                dispose();
+            } else {
+                JOptionPane.showMessageDialog(null, "Passwords does not match... Please try again.", "CRMS Registration", JOptionPane.WARNING_MESSAGE);
             }
         } else {
-            JOptionPane.showMessageDialog(null, "Passwords does not match... Please try again.", "CRMS Registration", JOptionPane.WARNING_MESSAGE);
+            JOptionPane.showMessageDialog(null, "Password/Username must not contain ':'... Please try again ", "CRMS Registration", JOptionPane.WARNING_MESSAGE);
         }
+        
     }//GEN-LAST:event_jButton1ActionPerformed
 
     private void jPasswordField1FocusGained(java.awt.event.FocusEvent evt) {//GEN-FIRST:event_jPasswordField1FocusGained
